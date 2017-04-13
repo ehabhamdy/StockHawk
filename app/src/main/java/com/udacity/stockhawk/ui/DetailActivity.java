414c2c7ebbbee4a3d5659d06d549460e60db2d68
ehabhamdy <ehabhamdy2012@gmail.com>
2017-04-13 22:06:34 +0200

    Adding Stock Widget

---
+++ app/src/main/java/com/udacity/stockhawk/ui/DetailActivity.java
@@ -0,0 +1,10 @@
+package com.udacity.stockhawk.ui;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by ehabhamdy on 4/13/17.
+ */
+
+public class DetailActivity extends AppCompatActivity{
+}
